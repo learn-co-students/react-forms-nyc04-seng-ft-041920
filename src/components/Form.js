c5cfa9340eb2199306f13841e5e75016724d643e
jinnic <yoohyojin@gmail.com>
2020-07-14 22:41:55 -0400

    Done.

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,27 +3,67 @@ import React from 'react';
 class Form extends React.Component {
   state = {
     firstName: "John",
-    lastName: "Henry"
+    lastName: "Henry",
+    check: false,
+    submittedData: []
   }
 
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
-    })
+    },()=> console.log(this.state))
   }
 
   handleLastNameChange = event => {
     this.setState({
       lastName: event.target.value
+    }, ()=> console.log(this.state))
+  }
+
+  handleChecked = event => {
+    this.setState({
+        check: event.target.checked
+    }, ()=> console.log(this.state))
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+    //since we don't have server we are mocking with below code
+    // this.sendFormDataSomewhere(formData)
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
+
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
     })
   }
 
   render() {
     return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input
+            type="text"
+            onChange={event => this.handleFirstNameChange(event)}
+            value={this.state.firstName}
+          />
+          <input
+            type="text"
+            onChange={event => this.handleLastNameChange(event)}
+            value={this.state.lastName}
+          />
+          <input 
+            type="checkbox"
+            onChange={event => this.handleChecked(event)}
+            checked={this.state.check}
+          />
+          <input type="submit"/>
+        </form>
+        {this.listOfSubmissions()}
+      </div>
     )
   }
 }
